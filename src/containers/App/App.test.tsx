c0bd27cacfadf9241110d27243fb158fea644197
rizikolik <ynsture@gmail.com>
2021-09-10 14:18:03 +0300

    update test

--- src/containers/App/App.test.tsx
+++ src/containers/App/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { queryAllByText } = render(<App />);
+  const lorems = queryAllByText('Lorem')[0];
+  expect(lorems).toBeInTheDocument();
 });
